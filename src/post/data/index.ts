87bb450d8c6949de94025429cda59169f9c43bd6
Erik Riquelme <erikone2311@gmail.com>
2024-11-13 22:52:48 +0100

    Create posts data

---
+++ src/post/data/index.ts
@@ -0,0 +1,19 @@
+import Post from "../Post/Post";
+
+const post1 = new Post(
+  "I like cats",
+  "",
+  "I like cats forever!",
+  "LoverCats04",
+);
+
+const post2 = new Post(
+  "I hate cats",
+  "",
+  "I hate cats forever!",
+  "HaterCats04",
+);
+
+const posts = [post1, post2];
+
+export default posts;
