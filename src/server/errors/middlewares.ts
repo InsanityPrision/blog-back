887f8694757500f7cc000aa1cf6d9b54b29cd5a4
Erik Riquelme <erikone2311@gmail.com>
2024-11-14 23:48:34 +0100

    Add errors middlewares

---
+++ src/server/errors/middlewares.ts
@@ -0,0 +1,27 @@
+import chalk from "chalk";
+import { type NextFunction, type Request, type Response } from "express";
+import ServerError from "./ServerError/ServerError.js";
+
+export const handlePathError = (
+  _req: Request,
+  _res: Response,
+  next: NextFunction,
+): void => {
+  const error = new ServerError(404, "Endpoint not found");
+  console.log("hola");
+  next(error);
+};
+
+export const handleErrors = (
+  error: ServerError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  console.log(chalk.red(error.message));
+  console.log(chalk.red(error.stack));
+
+  res
+    .status(error.statusCode ?? 500)
+    .json({ error: error.statusCode ? error.message : "Server error" });
+};
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import postRouter from "../post/router/index.js";
+import { handleErrors, handlePathError } from "./errors/middlewares.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use("/posts", postRouter);
+
+app.use("/", handlePathError);
+
+app.use(handleErrors);
 
 export default app;
