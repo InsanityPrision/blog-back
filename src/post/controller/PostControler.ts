e89aae4b84a1c585ec2c914becb2296f65490c24
Erik Riquelme <erikone2311@gmail.com>
2024-11-18 00:54:13 +0100

    Create post method for PostController

--- src/post/controller/PostControler.ts
+++ src/post/controller/PostControler.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from "express";
 import { type PostControllerStructure } from "./types";
-import type Post from "../Post/Post";
+import Post from "../Post/Post.js";
+import posts from "../data/index.js";
 
 class PostController implements PostControllerStructure {
   constructor(private readonly posts: Post[]) {}
@@ -9,6 +10,16 @@ class PostController implements PostControllerStructure {
     const statusCode = 200;
 
     res.status(statusCode).json({ posts: this.posts });
+  };
+
+  post = (req: Request, res: Response): void => {
+    const { title, author, content, imageUrl } = req.body as Post;
+
+    const newPost = new Post(title, imageUrl, content, author);
+
+    posts.push(newPost);
+
+    res.status(201).json({ post: newPost });
   };
 }
 
