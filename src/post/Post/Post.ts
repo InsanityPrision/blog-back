25314eb482668e4be155a00d6b8cc3d368220777
Erik Riquelme <erikone2311@gmail.com>
2024-11-20 16:53:25 +0100

    Create a inteface for PostImage

--- src/post/Post/Post.ts
+++ src/post/Post/Post.ts
@@ -1,19 +1,25 @@
 import crypto from "crypto";
-import { type PostStructure } from "./types";
+import { type PostImage, type PostStructure } from "./types";
 
 class Post implements PostStructure {
   public id: string;
   public date: Date;
+  public imageUrl: string;
+  public alternativeText: string;
 
   constructor(
     public title: string,
-    public imageUrl: string,
     public content: string,
     public author: string,
+    { imageUrl, alternativeText }: PostImage,
   ) {
     this.id = crypto.randomUUID();
 
     this.date = new Date(Date.now());
+
+    this.imageUrl = imageUrl;
+
+    this.alternativeText = alternativeText;
   }
 }
 
